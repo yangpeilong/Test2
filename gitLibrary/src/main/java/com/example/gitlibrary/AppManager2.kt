510e74107a28ba8f00510dbe4863d91c15683f3c
yangyeah <yangpeilong@qding.me>
2019-09-02 18:00:33 +0800

    二次提交

---
+++ gitLibrary/src/main/java/com/example/gitlibrary/AppManager2.kt
@@ -0,0 +1,19 @@
+package com.example.gitlibrary
+
+import android.content.Context
+
+/**
+ *  Created by yangyeah on 2019/9/2
+ *
+ *  describe:
+ */
+object AppManager {
+
+    fun init(app: Context) {
+
+    }
+
+    fun set() {
+
+    }
+}
